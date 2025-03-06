0f16c274d6062af2b52977a5bd2619e0ca1219c2
YoussefSayedDev <youssef.el.sayed.email@gmail.com>
2025-03-06 03:51:39 +0200

    Disable database seeding operation in prisma/seed/seed.ts

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { seedClassified } from "./classifieds.seed";
 import { seedImages } from "./images.seed";
-import { seedTaxnomy } from "./taxnomy.seed";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$executeRaw`TRUNCATE TABLE "makes", "models" RESTART IDENTITY CASCADE;`;
-  await seedTaxnomy(prisma);
-  await seedClassified(prisma);
+  // await prisma.$executeRaw`TRUNCATE TABLE "makes", "models" RESTART IDENTITY CASCADE;`;
+  // await seedTaxnomy(prisma);
+  // await seedClassified(prisma);
   await seedImages(prisma);
 }
 
